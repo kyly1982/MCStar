f55d6f4d2b5520558b2bf62fe6b40611e0ded351
kyly1982 <anloy_3633@sohu.com>
2015-10-20 09:48:12 +0800

    初始化

--- MCStar/app/src/main/java/com/mckuai/mcstar/utils/CircleBitmap.java
+++ MCStar/app/src/main/java/com/mckuai/mcstar/utils/CircleBitmap.java
@@ -1,23 +1,15 @@
 package com.mckuai.mcstar.utils;
 
-import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
-import android.graphics.Matrix;
 import android.graphics.Paint;
 import android.graphics.PorterDuff;
 import android.graphics.PorterDuffXfermode;
 import android.graphics.Rect;
-import android.graphics.RectF;
-import android.graphics.drawable.Drawable;
 import android.support.annotation.NonNull;
 
-import com.mckuai.mcstar.R;
-
-import java.nio.channels.NonWritableChannelException;
-
 /**
- * Created by kyly on 2015/10/19.
+ * Created by kyly on 2015/10/20.
  */
 public class CircleBitmap {
     private static int mBorderThickness = 0;
